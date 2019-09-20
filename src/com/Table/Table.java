31ed752491761c6fc51beb96fa7e847fd1e12c14
zhuyanmei823 <54883835+zhuyanmei823@users.noreply.github.com>
2019-09-20 15:15:28 +0800

    update
    add";"  , add " " between symbol

--- src/com/Table/Table.java
+++ src/com/Table/Table.java
@@ -1,10 +1,10 @@
 public class Table{
 	
 	public static void main(String[] args){
-		for(int i=1;i<=9;i++)
-			for(int j=1;j<=i;j++){
-				System.out.print(i+"*"+j+"="+j*1+" ");
+		for(int i = 1;i <= 9;i++)
+			for(int j = 1;j <= i;j++){
+				System.out.print(i + "*" + j + "=" + j*i + " ");
 			}
-			System.out.print("\n")
+			System.out.print("\n");
 	}
 }
